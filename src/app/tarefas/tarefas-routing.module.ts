ac90698a529a99bf27c1bf0ba913bc8966c1c238
Moraes <wmarcono@globo.com>
2020-04-06 11:46:08 -0300

    commit inicial

---
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+
+import { ListarTarefasComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+
+export const TarefaRoutes: Routes = [
+   
+    {
+        path: 'tarefas',
+        redirectTo: 'tarefas/listar'
+    },
+    {
+        path: 'listar',
+        component: ListarTarefasComponent
+    },
+    {
+        path: 'tarefas/listar',
+        component: ListarTarefasComponent
+    },
+    {
+        path: 'tarefas/cadastrar',
+        component: CadastrarTarefaComponent
+    },
+    {
+        path: 'tarefas/editar/:id',
+        component: EditarTarefaComponent
+    }
+];
---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ListarTarefasComponent } from './listar';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
+import { CadastrarTarefaComponent } from './cadastrar/cadastrar-tarefa.component';
+import { EditarTarefaComponent } from './editar/editar-tarefa.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListarTarefasComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers:[
+    TarefaService
+  ]
+})
+export class TarefasModule { }
